8f504b5ef50a46bc5eda203abe1014858b76ac9c
Simeone Vilardo <simeone.vilardo@gmail.com>
2016-11-09 19:58:36 +0100

    Enhance BubbleSort.

--- BubbleSort/BubbleSort.c
+++ BubbleSort/BubbleSort.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
-#define SIZE 8
+#define SIZE 10
 
-int main()
-{
-	char array[SIZE] = { 'f','u', 'r', 'v', 'h', 'g', 'z', 'c' };
-	int count, i;
+void pflush();
 
-	printf("Stampa array originale:\n");
-	for (i = 0; i < SIZE; i++)
-		printf("%c ", array[i]);
-
-	for (count = 1; count < SIZE; count++)
-	{
-		for (i = 0; i <SIZE-1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				char temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-			}
-		}
+int main() {
+	int arr[SIZE];
+	printf("Inserire 10 elementi nell'array\n");
+	for (int i = 0; i < SIZE; i++) {
+		printf("Inserire l'elemento numero %d: ", i + 1);
+		scanf("%d", &arr[i]);
 	}
-
-	printf("\nStampa array in ordine alfabetico:\n");
-
-	for (i = 0; i < SIZE; i++)
-		printf("%c ", array[i]);
+	int temp = 0;
+	int count = SIZE;
+	while (count-- > 0)
+		for (int j = 0; j < SIZE - 1; j++)
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+	printf("Il nuovo array ordinato e': ");
+	for (int i = 0; i < SIZE; i++)
+		printf("%d ", arr[i]);
+	pflush();
+	printf("\nPremi INVIO per terminare il programma\n");
 	getchar();
-
 	return 0;
+}
+
+void pflush() {
+	char c;
+	while (c = getchar() != '\n' && c != EOF) {}
 }
