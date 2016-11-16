8b06e05ededc81d24935d7125e77c1d524f65a5c
Simeone Vilardo <simeone.vilardo@gmail.com>
2016-11-16 19:09:05 +0100

    Add Esercizio12.

---
+++ Esercizio12/Esercizio12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define N 5
+
+typedef struct { char nome[100]; char docente[100]; int numeroCFU; int voto; } Esame;
+
+int main() {
+	Esame esami[N];
+	for (int i = 0; i < N; i++) {
+		printf("Inserire il nome del corso: ");
+		scanf("%s", &esami[i].nome);
+		printf("Inserire il cognome del docente: ");
+		scanf("%s", &esami[i].docente);
+		printf("Inserire il numero di CFU: ");
+		scanf("%d", &esami[i].numeroCFU);
+		printf("Inserire il voto: ");
+		scanf("%d", &esami[i].voto);
+	}
+	int somma = 0, totCFU = 0;
+	for (int i = 0; i < N; i++) {
+		somma += esami[i].voto * esami[i].numeroCFU;
+		totCFU += esami[i].numeroCFU;
+		printf("Il nome del corso e': %s\n", esami[i].nome);
+		printf("Il cognome del docente e': %s\n", esami[i].docente);
+		printf("Il numero di CFU e': %d\n", esami[i].numeroCFU);
+		printf("Il voto del'esame e': %d\n", esami[i].voto);
+	}
+	printf("La media e': %.1f\n", ((float)somma) / totCFU);
+	return 0;
+}
