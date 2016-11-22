258cc2b8f14f875541fef823b2402aa63698a0c8
Simeone Vilardo <simeone.vilardo@gmail.com>
2016-11-23 00:43:29 +0100

    Adapt code to exercise's requirements.

--- Esercizio15/Esercizio15.c
+++ Esercizio15/Esercizio15.c
@@ -3,26 +3,31 @@
 #define SIZE 10
 #define SUM_SIZE SIZE*2
 
+typedef struct { int checks; int swaps; } Stat;
+
 void pflush();
 void setArray(int arr[], int size, char* text);
-void bubbleSort(int arr[], int size);
+Stat bubbleSort(int arr[], int size);
 void printArray(int arr[], int size, char* text);
 void sumArrays(int arr1[], int arr2[], int size, int sumArr[], int sumSize);
+void swap(int arr[], int i, int j);
 
 int main() {
 	int arr1[SIZE];
 	int arr2[SIZE];
 	int sumArr[SUM_SIZE];
-	setArray(arr1, "Inserire gli elementi del primo array", SIZE);
-	setArray(arr2, "Inserire gli elementi del secondo array", SIZE);
+	setArray(arr1, SIZE, "Inserire gli elementi del primo array");
+	setArray(arr2, SIZE, "Inserire gli elementi del secondo array");
 	printArray(arr1, SIZE, "Il primo array originale e':");
 	printArray(arr2, SIZE, "Il secondo array originale e':");
-	bubbleSort(arr1, SIZE);
-	bubbleSort(arr2, SIZE);
+	Stat stat1 = bubbleSort(arr1, SIZE);
+	Stat stat2 = bubbleSort(arr2, SIZE);
+	Stat stat = { stat1.checks + stat2.checks, stat2.swaps + stat2.swaps };
 	printArray(arr1, SIZE, "Il primo array ordinato e':");
 	printArray(arr2, SIZE, "Il secondo array ordinato e':");
 	sumArrays(arr1, arr2, SIZE, sumArr, SUM_SIZE);
 	printArray(sumArr, SUM_SIZE, "La somma ordinata degli array e': ");
+	printf("Numero totale di confronti: %d\nNumero totale di ordinamenti: %d\n", stat.checks, stat.swaps);
 	pflush();
 	getchar();
 	return 0;
@@ -36,15 +41,25 @@ void setArray(int arr[], int size, char* text) {
 	}
 }
 
-void bubbleSort(int arr[], int size) {
-	int temp = 0;
-	for (int i = 0; i < size; i++)
-		for (int j = 0; j < size; j++)
-			if (arr[j] > arr[i]) {
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;;
+Stat bubbleSort(int arr[], int size) {
+	Stat stat = { 0,0 };
+	int count = SIZE - 1;
+	while (count-- > 0)
+		for (int i = 0; i < SIZE - 1; i++) {
+			stat.checks++;
+			if (arr[i] > arr[i + 1]) {
+				stat.swaps++;
+				swap(arr, i, i + 1);
 			}
+		}
+	return stat;
+}
+
+void swap(int arr[], int i, int j) {
+	int temp = 0;
+	temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;;
 }
 
 void sumArrays(int arr1[], int arr2[], int size, int sumArr[], int sumSize) {
