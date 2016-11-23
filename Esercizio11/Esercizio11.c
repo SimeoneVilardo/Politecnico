67234558d7b2e800a87f2e7a18e83bced1201644
Simeone Vilardo <SimeoneVilardo@users.noreply.github.com>
2016-11-23 16:46:30 +0100

    Update Esercizio11.c
    Syntax change for Laura.

--- Esercizio11/Esercizio11.c
+++ Esercizio11/Esercizio11.c
@@ -10,7 +10,7 @@ int main() {
 	int root = (int)sqrt(K);
 	for (int i = 0; i < root; i++)
 		for (int j = i + 1; j < K; j++)
-			if (setaccio[i] != 0 && !(setaccio[j] % setaccio[i]))
+			if (setaccio[i] != 0 && (setaccio[j] % setaccio[i]) == 0)
 				setaccio[j] = 0;
 	for (int i = 0; i < K; i++)
 		if (setaccio[i])
