89d6e3ac6d3d9acfe0ff0286413440afab949506
Simeone Vilardo <simeone.vilardo@gmail.com>
2016-12-11 21:23:18 +0100

    Add Kaprekar to Digits.

--- Digits/Digits.c
+++ Digits/Digits.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #define DIGITS 4
 #define MAX (int)(pow(10, DIGITS) - 1)
+#define KAPREKAR_CONST 6174
 
 typedef struct { int* values; int size; } Array;
 
@@ -11,8 +12,13 @@ Array convertIntToArray(int, int);
 int getLargestInArray(Array*);
 int countDigits(int);;
 Array getDescendingDigits(int);
-void sortArray(Array*);
+Array getAscendingDigits(int);
+void bubbleSort(Array*);
+void reverseBubbleSort(Array*);
 void swap(int*, int, int);
+void getInputNum(int *);
+int getKaprekar(int);
+int concatenateInts(int, int);
 void pflush();
 
 int main() {
@@ -24,6 +30,7 @@ int main() {
 		printf("\nMenu Principale:\n");
 		printf("  1) Trova la cifra piu' grande in un intero\n");
 		printf("  2) Ordina le cifre di un intero in ordine decrescente\n");
+		printf("  3) Calcola le iterazioni di Kaprekar\n");
 		printf("  99) Chiudi\n");
 		printf("\nNumero operazione: ");
 		scanf("%d", &op);
@@ -31,26 +38,21 @@ int main() {
 		switch (op)
 		{
 		case 1:
-			while (num < 0 || num > MAX)
-			{
-				printf("Inserisci un numero compreso tra 0 e %d: ", MAX);
-				scanf("%d", &num);
-			}
+			getInputNum(&num);
 			int largest = getLargestDigit(num);
 			printf("La cifra piu' grande in %d e' %d", num, largest);
-			num = -1;
 			break;
 		case 2:
-			while (num < 0 || num > MAX)
-			{
-				printf("Inserisci un numero compreso tra 0 e %d: ", MAX);
-				scanf("%d", &num);
-			}
+			getInputNum(&num);
 			Array arr = getDescendingDigits(num);
 			printf("Le cifre di %d in ordine decrescente sono: ", num);
 			for (int i = 0; i < arr.size; i++)
 				printf("%d", arr.values[i]);
-			num = -1;
+			break;
+		case 3:
+			getInputNum(&num);
+			int iterations = getKaprekar(num);
+			printf("Il numero di iterazioni di Kaprekar del numero %d e' %d", num, iterations);
 			break;
 		case 99:
 			printf("L'applicazione sara' terminata");
@@ -59,11 +61,39 @@ int main() {
 			printf("Comando sconosciuto");
 			break;
 		}
+		num = -1;
 		pflush();
 		printf("\n\nPremi INVIO per continuare...");
 		getchar();
 	} while (op != 99);
 	return 0;
+}
+
+void getInputNum(int *num) {
+	while (*num < 0 || *num > MAX)
+	{
+		printf("Inserisci un numero compreso tra 0 e %d: ", MAX);
+		scanf("%d", num);
+	}
+}
+
+int getKaprekar(int num) {
+	int index = 0;
+	while (num != KAPREKAR_CONST)
+	{
+		Array descArr = getDescendingDigits(num);
+		Array ascArr = getAscendingDigits(num);
+		int descNum = descArr.values[0];
+		int ascNum = ascArr.values[0];
+		for (int i = 1; i < descArr.size; i++) {
+			descNum = concatenateInts(descNum, descArr.values[i]);
+			ascNum = concatenateInts(ascNum, ascArr.values[i]);
+		}
+		num = descNum - ascNum;
+		printf("%d - %d = %d\n", descNum, ascNum, num);
+		index++;
+	}
+	return index;
 }
 
 int countDigits(int num) {
@@ -80,11 +110,24 @@ int getLargestDigit(int num) {
 
 Array getDescendingDigits(int num) {
 	Array arr = convertIntToArray(num, DIGITS);
-	sortArray(&arr);
+	reverseBubbleSort(&arr);
 	return arr;
 }
 
-void sortArray(Array* arr) {
+Array getAscendingDigits(int num) {
+	Array arr = convertIntToArray(num, DIGITS);
+	bubbleSort(&arr);
+	return arr;
+}
+
+void bubbleSort(Array* arr) {
+	for (int j = 0; j < arr->size - 1; j++)
+		for (int i = 0; i < arr->size - 1; i++)
+			if (arr->values[i] > arr->values[i + 1])
+				swap(arr->values, i, i + 1);
+}
+
+void reverseBubbleSort(Array* arr) {
 	for (int j = 0; j < arr->size - 1; j++)
 		for (int i = 0; i < arr->size - 1; i++)
 			if (arr->values[i] < arr->values[i + 1])
@@ -112,6 +155,13 @@ Array convertIntToArray(int num, int len) {
 	for (int i = 0; i < len; i++)
 		arr[len - 1 - i] = (num / (int)pow(10, i)) % 10;
 	return (Array) { arr, len };
+}
+
+int concatenateInts(int x, int y) {
+	int p = 10;
+	while (y >= p)
+		p *= 10;
+	return x * p + y;
 }
 
 void pflush() {
