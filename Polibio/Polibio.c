e68146dd94805853337bcd02ed8a14d3f31c92b3
Simeone Vilardo <simeone.vilardo@gmail.com>
2017-01-05 16:40:56 +0100

    Refactor code.

--- Polibio/Polibio.c
+++ Polibio/Polibio.c
@@ -3,7 +3,6 @@
 #define N 27
 #define KEY_SIZE 5
 #define PATH_SIZE 128
-#define WHITESPACE ' '
 
 int main() {
 	char key[][5] = { {'a', 'b', 'c', 'd', 'e'},{ 'f', 'g', 'h', 'i', 'k' },{ 'l', 'm', 'n', 'o', 'p' },{ 'q', 'r', 's', 't', 'u' },{ 'v', 'w', 'x', 'y', 'z' } };
@@ -21,17 +20,11 @@ int main() {
 }
 
 int polybius(FILE *fp, char key[][KEY_SIZE], int* cifratura) {
+	int index = 0;
 	char c;
-	int index = -1;
 	while ((c = getc(fp)) != EOF) {
-		index++;
-		if (c == WHITESPACE)
-		{
-			cifratura[index] = 0;
-			continue;
-		}
-		if (c == 'j')
-			c = 'i';
+		cifratura[index] = 0;
+		if (c == 'j') c = 'i';
 		int found = 0;
 		for (int i = 0; i < KEY_SIZE && !found; i++)
 			for (int j = 0; j < KEY_SIZE && !found; j++)
@@ -39,5 +32,6 @@ int polybius(FILE *fp, char key[][KEY_SIZE], int* cifratura) {
 					cifratura[index] = (j + 1) + ((i + 1) * 10);
 					found = 1;
 				}
+		index++;
 	}
 }
